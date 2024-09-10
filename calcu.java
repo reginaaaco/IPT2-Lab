e6c354e6f76e201b3bdc006d8d43e3cd5e6632bd
regina <reginaco257@gmail.com>
2024-09-10 21:41:06 +0800

    multiplication

--- calcu.java
+++ calcu.java
@@ -8,9 +8,15 @@ public class calcu {
         return a - b;
     }
 
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+
     public static void main(String[] args) {
         System.out.println("Addition: " + add(5, 3));
         System.out.println("Subtraction: " + subtract(5, 3));
+        System.out.println("Multiplication: " + multiply(5, 3));
 
     }
 }
