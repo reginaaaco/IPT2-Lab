ba4f4d1a4559b98b18dbec0f8c48f62141aa391e
regina <reginaco257@gmail.com>
2024-09-10 21:38:06 +0800

    subtraction

--- calcu.java
+++ calcu.java
@@ -3,7 +3,14 @@ public class calcu {
     public static int add(int a, int b) {
         return a + b;
     }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
     public static void main(String[] args) {
         System.out.println("Addition: " + add(5, 3));
+        System.out.println("Subtraction: " + subtract(5, 3));
+
     }
 }
