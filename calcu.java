ae163037984193a985a2120749f38c9a1b46b006
regina <reginaco257@gmail.com>
2024-09-10 21:33:23 +0800

    addition

---
+++ calcu.java
@@ -0,0 +1,9 @@
+public class calcu {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+    public static void main(String[] args) {
+        System.out.println("Addition: " + add(5, 3));
+    }
+}
